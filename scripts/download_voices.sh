4d3409cb74056b95d734c6506dc1f121d1f62432
Shidfar Hodizoda <shidfar@blackbox.com>
2025-11-19 13:35:13 +0100

    fix scripts for my mac

--- scripts/download_voices.sh
+++ scripts/download_voices.sh
@@ -1,2 +1,2 @@
 mkdir data
-wget "https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files-v1.0/voices-v1.0.bin" -O "data/voices-v1.0.bin"
+curl https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files-v1.0/voices-v1.0.bin > "data/voices-v1.0.bin"
